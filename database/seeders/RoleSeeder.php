38d1a8dacfbf26617e7cce1ba407b8e946c28550
Aksal <aksalsyah@refactory.id>
2022-07-22 01:35:26 +0700

    fix: enforce role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
 
 class RoleSeeder extends Seeder
 {
@@ -16,12 +17,15 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
+        Schema::disableForeignKeyConstraints();
+        Role::truncate();
 
         $roles = [
             ["name" => "admin", "created_at" => Carbon::now(), 'updated_at' => Carbon::now()],
             ["name" => "applicant", "created_at" => Carbon::now(), 'updated_at' => Carbon::now()]
         ];
 
-        Role::insert($roles);
+        Role::updateOrCreate($roles);
+        Schema::enableForeignKeyConstraints();
     }
 }
